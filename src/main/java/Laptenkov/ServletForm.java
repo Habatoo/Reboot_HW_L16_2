06bc084b17ab7fb7c7d0f553aa4142793acd80be
habatoo <habatoo@gmail.com>
2021-09-26 15:16:11 +0700

    initial commit

---
+++ src/main/java/Laptenkov/ServletForm.java
@@ -0,0 +1,13 @@
+package Laptenkov;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Сервлет - работает с формой (два текстовых поля)
+ * и позволяет добавлять посты в ленте и отображать их после
+ * отправки формы
+ * @author habatoo
+ */
+@WebServlet("/form")
+public class ServletForm {
+}
